d80ebbf4cd9cf6ece0cf87b780a90eb39cded00c
AirKong <87849070+AirKong@users.noreply.github.com>
2022-07-09 16:39:39 +0800

    Add files via upload

---
+++ echartsLoopTestFile/options/mainRose.js
@@ -0,0 +1,27 @@
+const roseOption = {
+    series: [
+        {
+            name: 'Nightingale Chart',
+            type: 'pie',
+            radius: [50, 250],
+            center: ['50%', '50%'],
+            roseType: 'area',
+            itemStyle: {
+                borderRadius: 8
+            },
+            label: {
+                show: false
+            },
+            data: [
+                { value: 40, name: 'rose 1' },
+                { value: 38, name: 'rose 2' },
+                { value: 32, name: 'rose 3' },
+                { value: 30, name: 'rose 4' },
+                { value: 28, name: 'rose 5' },
+                { value: 26, name: 'rose 6' },
+                { value: 22, name: 'rose 7' },
+                { value: 18, name: 'rose 8' }
+            ]
+        }
+    ]
+};
